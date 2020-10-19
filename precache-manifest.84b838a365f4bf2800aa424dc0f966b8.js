ec4fa47853978b6dee0899a96e5c0aee9a1649f8
sungjinahn <fourwings@naver.com>
2020-10-19 20:54:35 +0900

    Updates

---
+++ precache-manifest.84b838a365f4bf2800aa424dc0f966b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69cb8e0bc8c2e960e8a254ed212e281d",
+    "url": "/react_movie_2020/index.html"
+  },
+  {
+    "revision": "ff37a695cea204acb8e2",
+    "url": "/react_movie_2020/static/css/main.43ae2a1b.chunk.css"
+  },
+  {
+    "revision": "4098296b5788488f6cda",
+    "url": "/react_movie_2020/static/js/2.fe4da22c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react_movie_2020/static/js/2.fe4da22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff37a695cea204acb8e2",
+    "url": "/react_movie_2020/static/js/main.1232b48f.chunk.js"
+  },
+  {
+    "revision": "3637891d5ca0a73e096b",
+    "url": "/react_movie_2020/static/js/runtime-main.a3aebe02.js"
+  }
+]);
